fbd5897438273e32fa95103d8ab906c52a7d7459
Hafidz Zakky D <hafidzzakkyd@gmail.com>
2016-01-22 00:25:42 +0700

    kontak+footer nih

---
+++ kontak.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kontak Perusahaan</title>
+    <link href="asset/css/bootstrap.min.css" rel="stylesheet">
+    <link href="asset/css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+    <h3>KONTAK KAMI</h3>
+    <hr>
+      <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+          <div class="col-md-3">
+            <br>
+            <img src="img/logo.png" class="img-responsive">
+            <br>
+            <span class="glyphicon glyphicon-map-marker"> Lokasi</span>
+            <br>
+            <h5>Jl. Sadewa I No.59</h5>
+            <br>
+            <span class="glyphicon glyphicon-phone-alt"> Telepon</span>
+            <h5>+62 85642334490</h5>
+            <br>
+            <span class="glyphicon glyphicon-envelope"> Email</span>
+            <h5>websitemagang@gmail.com</h5>
+          </div>
+          <div class="col-md-9">
+            <p><h1>WebsiteName</h1> adalah Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <br>
+            <hr>
+            <br>
+            <div class="row">
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="img/logo.png" alt="gambar">
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="img/logo.png" alt="gambar">
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="img/logo.png" alt="gambar">
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="img/logo.png" alt="gambar">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>    
+    </div>
+   <!--footer-->
+      <div class="bottom">
+          <div class="container">
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-home"></span> Nama Perusahaan</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                     tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                  <p><a href="">continue reading -></a></p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-briefcase"></span> Magang</h4>
+                  <p>Web Apps</p>
+                  <p>Mobile Apps</p>
+                  <p>Desktop Apps</p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-flag"></span> Perusahaan</h4>
+                  <p>Perusahaan 1</p>
+                  <p>Perusahaan 2</p>
+                  <p>Perusahaan 3</p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-menu-right"></span> Ikuti Kami</h4>
+                  <p>facebook</p>
+                  <p>twitter</p>
+                  <p>instagram</p>
+                  <hr>
+              </div>
+          </div>
+      </div>
+      <div class="bawah">
+        <div class="container">
+          <p>magang</p>
+        </div>
+      </div>
+    <!--end footer-->  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+  </body>
+</html>
