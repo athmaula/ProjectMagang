c7265d2d9eac2d496c265bfe355af8dcf7e9040b
Hafidz Zakky D <hafidzzakkyd@gmail.com>
2016-01-22 20:23:56 +0700

    footer broo

---
+++ footer.php
@@ -0,0 +1,39 @@
+        <div class="bottom">
+          <div class="container-fluid">
+              <div class="col-md-3 col-sm-4">
+                  <h4><i class="fa fa-building"> Nama Perusahaan</i></h4>
+                  <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.  
+                  </p>
+                  <p><a class="btn btn-success" href="">continue reading</a></p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-briefcase"> Magang</span></h4>
+                  <p>Web Apps</p>
+                  <p>Mobile Apps</p>
+                  <p>Desktop Apps</p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-flag"> Perusahaan</span></h4>
+                  <p>Perusahaan 1</p>
+                  <p>Perusahaan 2</p>
+                  <p>Perusahaan 3</p>
+                  <hr>
+              </div>
+              <div class="col-md-3 col-sm-4">
+                  <h4><span class="glyphicon glyphicon-menu-right"> Ikuti Kami</span></h4>
+          <p><i class="fa fa-facebook-official"> facebook</i></p>
+                  <p><i class="fa fa-twitter-square"> twitter</i></p>
+                  <p><i class="fa fa-instagram"> instagram</i></p>
+                  <hr>
+              </div>
+          </div>
+      </div>
+      <div class="bawah">
+        <div class="container-fluid">
+          <p><i class="fa fa-copyright">magang</i></p>
+        </div>
+      </div>
