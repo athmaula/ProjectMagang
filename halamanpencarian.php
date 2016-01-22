c6f483316bb715b5edf9addf8633d4c7ed27649c
athoil <athoil286@gmail.com>
2016-01-22 18:02:34 +0700

    tambahan file halsearch, header

---
+++ halamanpencarian.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Pencarian</title>
+</head>
+<body>
+<div class="container">
+	<div class="content">
+		<form class="navbar-right">
+			<div class="form-group">
+				<input class="form-control" type="text" placeholder="Search">	
+			</div>
+		</form>
+	<div class="dropdown">
+  		<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Cari berdasarkan<span class="caret"></span></button>
+  			<ul class="dropdown-menu">
+			    <li><a href="#">Perusahaan</a></li>
+			    <li><a href="#">Pekerjaan</a></li>
+			</ul>
+	</div>
+	</div>
+<div class="content">
+	<div class="row">
+		<div class="col-xs-12 col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+  						<div class="panel-heading">
+					    	<h1 class="panel-title">Nama Perusahaan</h1>
+						</div>
+					<div class="panel-body">
+						<div class="col-xs-12 col-md-3">
+							<img class="img-responsive" src="elementary-logo-blue.sh.png">
+					</div>
+					<div class="col-xs-12 col-md-8">
+						<table class="table table-condensed">
+							<thead>
+								<strong>Nama Program Magang</strong>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a href="#"><button type="button" class="btn btn-success">Lebih Lengkap</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="content">
+	<div class="row">
+		<div class="col-xs-12 col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+  						<div class="panel-heading">
+					    	<h1 class="panel-title">Nama Perusahaan</h1>
+						</div>
+					<div class="panel-body">
+						<div class="col-xs-12 col-md-3">
+							<img class="img-responsive" src="elementary-logo-blue.sh.png">
+					</div>
+					<div class="col-xs-12 col-md-7">
+						<table class="table table-condensed">
+							<thead>
+								<strong>Nama Program Magang</strong>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+										Anggap saja ini desskripsi tempat magang
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a href="#"><button type="button" class="btn btn-success">Lebih Lengkap</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+	<div align="center">
+		 <ul class="pagination">
+		  <li><a href="#">1</a></li>
+		  <li><a href="#">2</a></li>
+		  <li><a href="#">3</a></li>
+		  <li><a href="#">4</a></li>
+		  <li><a href="#">5</a></li>
+		</ul>
+	</div>	
+</div>
+</body>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ <script src="asset/js/bootstrap.min.js"></script>
+ <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="style.css">
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website Name</title>
+	<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+    		<div class="navbar-header">
+      			<a class="navbar-brand" href="#">WebSiteName</a>
+    		</div>
+    		<ul class="nav navbar-nav">
+    			<li><a href="#">Beranda</a></li>
+      			<li><a href="#">Perusahaan</a></li>
+      			<li><a href="#">Galeri</a></li>
+      			<li><a href="#">Cari Magang</a></li>
+      			<li><a href="#">Kontak Kami</a></li>
+      		</ul>
+      	</div>
+     </nav>
+</body>
+</html>
